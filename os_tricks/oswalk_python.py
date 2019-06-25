5ecc03b7c99248ecbc02ad4dcd4e8d346730087c
Julio Cesar Da Silva <jdasilva@esrf.fr>
2019-06-25 15:34:05 +0200

    initial commit on branch

--- os_tricks/oswalk_python.py
+++ os_tricks/oswalk_python.py
@@ -27,29 +27,34 @@ full_file_paths = get_filepaths("/Users/johnny/Desktop/TEST")
 #You can use glob:
 
 import glob, os
-os.chdir("/mydir")
+os.chdir("./mydir")
 for file in glob.glob("*.txt"):
     print(file)
+os.chdir("..")
 
 #or simply os.listdir:
 
 import os
-for file in os.listdir("/mydir"):
+for file in os.listdir("./mydir"):
     if file.endswith(".txt"):
         print(file)
 
 #or if you want to traverse directory, use os.walk:
 
 import os
-for root, dirs, files in os.walk("/mydir"):
+for root, dirs, files in os.walk("./mydir"):
     for file in files:
         if file.endswith(".txt"):
              print(os.path.join(root, file))
 
 
 # another one
+directory = "./mydir"
 for root, dirs, files in os.walk(directory):
+    print("root: {}".format(root))
+    print("dirs: {}".format(dirs))
+    print("files: {}".format(files))
     for file in files:
         if file.endswith('.txt'):
-            print file
+            print(file)
 
